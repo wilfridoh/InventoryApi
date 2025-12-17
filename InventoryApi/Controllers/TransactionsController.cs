7ed5e39f02aa3855eb3a45dc273d55914015d1a1
claudihidal <claudihidal@gmail.com>
2025-12-17 00:15:33 -0500

    Fix: Se actualiza el controlador de transacciones para que retorne la consulta de forma estandar.

--- InventoryApi/Controllers/TransactionsController.cs
+++ InventoryApi/Controllers/TransactionsController.cs
@@ -1,5 +1,6 @@
 ﻿using InventoryApi.Aplicacion.Interfaces;
 using InventoryApi.Domain.Entities;
+using InventoryApi.API.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -20,18 +21,18 @@ namespace InventoryApi.API.Controllers
         public async Task<IActionResult> GetAll(int? productoId = null, DateTime? fromDate = null, DateTime? toDate = null, string? tipo = null)
         {
             var items = await _repo.GetAllAsync(productoId, fromDate, toDate, tipo);
-            return Ok(items);
+            return Ok(ResponseBuilder.Build(200, new { transacciones = items }));
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] Transaction transaction)
         {
-            if (transaction is null) return BadRequest();
+            if (transaction is null) return BadRequest(ResponseBuilder.Build(400, (object?)null, new[] { "El cuerpo de la petición es requerido y debe ser JSON válido." }));
 
             var result = await _repo.AddAsync(transaction);
 
             // Devuelve la transacción enriquecida (incluye ProductoNombre y StockActual)
-            return CreatedAtAction(nameof(GetAll), new { id = result.TransaccionId }, result);
+            return CreatedAtAction(nameof(GetAll), new { id = result.TransaccionId }, ResponseBuilder.Build(201, new { transacciones = new[] { result } }));
         }
     }
 }
